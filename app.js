2b86707ad8bac9c32dffc75a181c00eaaaa38aa8
John Tajima <manjiro@gmail.com>
2015-02-28 21:51:29 -0500

    fix deprecated app.configure()

--- app.js
+++ app.js
@@ -21,15 +21,16 @@ process.on('SIGINT', function () {
 
 // web service
 var app = express();
-app.configure(function(){
-  app.use(express.static(__dirname + '/public'))
-  app.use(app.router);
-  app.set('rasterizerService', new RasterizerService(config.rasterizer).startService());
-  app.set('fileCleanerService', new FileCleanerService(config.cache.lifetime));
-});
-app.configure('development', function() {
+
+app.use(express.static(__dirname + '/public'))
+app.use(app.router);
+app.set('rasterizerService', new RasterizerService(config.rasterizer).startService());
+app.set('fileCleanerService', new FileCleanerService(config.cache.lifetime));
+
+if (app.get('env') == 'development') {
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+};
+
 require('./routes')(app, config.server.useCors);
 var port = process.env.PORT || config.server.port || 3000;
 app.listen(port);
